0507b8b02e538924650cac95bb7a181e14aec50b
suragch <studymongolian@gmail.com>
2020-03-06 19:00:59 +0800

    update Flutter web version, menksoft buttons

---
+++ mongolcode/flutter_service_worker.js
@@ -0,0 +1,36 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "89c18e896f2b67ff0db9e5378d4b9e3d",
+"/main.dart.js": "cc194aac6869ca6d939a25fecdc00d04",
+"/assets/LICENSE": "433212f9c3d42c5f33505f202d93d3aa",
+"/assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request, {
+          credentials: 'include'
+        });
+      })
+  );
+});
